9da15e7d3f5e9931b944dbfd5a8e294a5e053305
Pedro Fonseca Nunes <pedrofonunes@gmail.com>
2024-09-11 19:28:53 -0300

    feat: ajustando projeto para CRUD.

--- src/main/java/demo/user/service/controller/UserController.java
+++ src/main/java/demo/user/service/controller/UserController.java
@@ -1,17 +1,51 @@
 package demo.user.service.controller;
 
+import demo.user.service.dto.UserRequestDTO;
 import demo.user.service.entity.User;
+import demo.user.service.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> findAll() {
+        return userService.findAll();
+    }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable("id") Long id) {
-        return new User(id, "User " + id, "user" + id + "@example.com");
+        return userService.findById(id);
+    }
+
+    @PostMapping
+    public User create(@Valid @RequestBody UserRequestDTO requestDTO) {
+        return userService.create(requestDTO);
+    }
+
+    @PutMapping("/{id}")
+    public User update(@PathVariable("id") Long id, UserRequestDTO requestDTO) {
+        return userService.update(id, requestDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public String delete(@PathVariable("id") Long id) {
+        userService.delete(id);
+        return "User excluído";
     }
 }
